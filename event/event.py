d8c5dcb63a00beb48ea36f85d19891fc419033c1
Mayu Laierlence <mayuki.minano@gmail.com>
2013-04-09 16:38:02 +0900

    Update event.py

--- event/event.py
+++ event/event.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 """
 Copyright (c) 2012-2013 Minacle
 
@@ -27,34 +25,58 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 class event(object):
 	
+	"""Initialise new Eeent object.
+		connected = event()
+	"""
 	def __init__(self):
 		self.__handlers__ = []
 	
+	"""Add event handler to event object.
+		connected += on_connected
+	"""
 	def __iadd__(self, handler):
 		self.__handlers__.append(handler)
 		return self
-		
+	
+	"""Remove event handler from event object.
+		connected -= on_connected
+	"""
 	def __isub__(self, handler):
 		self.__handlers__.remove(handler)
 		return self
 	
+	"""
+		connected &= on_connected
+	"""
 	def __iand__(self, handler):
 		self.__handlers__ = [handler] if handler else []
 		return self
 	
+	"""
+		connected |= on_connected
+	"""
 	def __ior__(self, handler):
 		if handler in self.__handlers__:
 			return self
 		return self.__iadd__(handler)
 	
+	"""
+		connected ^= on_connected
+	"""
 	def __ixor__(self, handler):
 		if handler in self.__handlers__:
 			self.__handlers__.remove(handler)
 		return self
 	
+	"""
+		on_connected in connected
+	"""
 	def __contains__(self, handler):
 		return True if handler in self.__handlers__ else False
 	
+	"""
+		connected()
+	"""
 	def __call__(self, *args, **kwargs):
 		for handler in self.__handlers__:
 			try:
@@ -62,6 +84,10 @@ class event(object):
 			except:
 				continue
 	
+	"""
+		@connected.handler
+		def on_connected(): pass
+	"""
 	def handler(self, func):
 		self.__ior__(func)
 
